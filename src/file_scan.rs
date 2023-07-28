ac2167a06558de3baffc6a1d5a14be57f719163a
Orkun Koçyiğit <orkunkocyigit@gmx.com>
2023-07-28 12:12:32 +0300

    fix: file_scan.rs panic in case file has no extensions - Added support for rust release via github actions

--- src/file_scan.rs
+++ src/file_scan.rs
@@ -3,12 +3,10 @@ use std::io;
 use std::path::{Path, PathBuf};
 
 pub fn read_files<P: AsRef<Path>>(path: P) -> Result<Vec<PathBuf>, Error> {
-    let result = path.as_ref().read_dir()?.filter_map(|e| e.ok());
     let mut paths = Vec::new();
-    for entry in result {
+    for entry in path.as_ref().read_dir()?.filter_map(|e| e.ok()) {
         let path = entry.path();
-        if path.is_file() && VALID_EXTENSIONS.contains(&path.extension().unwrap().to_str().unwrap())
-        {
+        if path.is_file() && is_extension_valid(&path) {
             paths.push(path);
         } else if path.is_dir() {
             paths.extend(read_files(path)?);
@@ -18,3 +16,9 @@ pub fn read_files<P: AsRef<Path>>(path: P) -> Result<Vec<PathBuf>, Error> {
 }
 
 const VALID_EXTENSIONS: [&str; 3] = ["zip", "rar", "7z"];
+
+fn is_extension_valid(path: &PathBuf) -> bool {
+    path.extension()
+        .and_then(|e| e.to_str())
+        .map_or(false, |e| VALID_EXTENSIONS.contains(&e))
+}
